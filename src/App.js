e39a4288cf7d222e15e26b547bafaf1b45a2bc9e
Hoàng <hoanghung20030@gmail.com>
2024-05-18 23:15:40 +0700

    remove StrictMode, create User and UserItem component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Navbar from './components/layouts/Navbar';
+import User from './components/users/User';
 function App() {
+  const [users,setUsers] = useState([]);
+  // dùng useEffect để handle axios get api 
+  useEffect(()=>{
+    // dùng hàm xử lý bất đồng bộ asynchronous để xử lý việc get api từ github
+    const fetchData = async ()=>{
+        try {
+          // dùng phương thức get của thư viện axios để lấy data từ api user của github
+          const response = await axios.get("https://api.github.com/users");
+          // dùng log để thực hiện debugger 
+          setUsers(response.data)
+        } catch (error) {
+          // log lỗi nếu có lỗi trong khi get data
+          console.error("Error fetching data", error);
+        }
+    }
+    // gọi lại hàm fetchData để thực thi hàm dã được định nghĩa trên
+    fetchData();
+  },[])
   return (
     <div className='App' >
       <Navbar />
       <div className='container'>
         <h1>Hello from react</h1>
       </div>
+      <User users={users}/>
     </div>
   )
 }
