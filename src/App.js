d06ff5a514cce657d106d95892db0befc09b2f54
Hoàng <hoanghung20030@gmail.com>
2024-05-19 00:59:36 +0700

    Create new page and link to another pages with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,35 +1,24 @@
 import './App.css';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
 import Navbar from './components/layouts/Navbar';
-import User from './components/users/User';
 import Search from './components/users/Search';
+import About from './components/pages/About';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import NotFound from './components/pages/NotFound';
 function App() {
-  const [users,setUsers] = useState([]);
-  // dùng useEffect để handle axios get api 
-  useEffect(()=>{
-    // dùng hàm xử lý bất đồng bộ asynchronous để xử lý việc get api từ github
-    const fetchData = async ()=>{
-        try {
-          // dùng phương thức get của thư viện axios để lấy data từ api user của github
-          const response = await axios.get("https://api.github.com/users");
-          // dùng log để thực hiện debugger 
-          setUsers(response.data)
-        } catch (error) {
-          // log lỗi nếu có lỗi trong khi get data
-          console.error("Error fetching data", error);
-        }
-    }
-    // gọi lại hàm fetchData để thực thi hàm dã được định nghĩa trên
-    fetchData();
-  },[])
   return (
     <div className='App' >
+      <Router>
       <Navbar />
-      <div className='container'>
-        <h1>Github Users Data</h1>
-        <Search />
-      </div>
+        <div className='container'>
+          <h1>Github Users Data</h1>
+          <Switch>
+            <Route exact path="/" component={Search} />
+            <Route exact path="/about" component={About} />
+            <Route path='/*' component={NotFound}></Route>
+          </Switch>
+        </div>
+
+      </Router>
     </div>
   )
 }
